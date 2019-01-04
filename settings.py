23e6ab42229d53b3aa79c2102cf4d4113c0f8108
Luciano Dos Santos <Luciano_z7@hotmail.com>
2019-01-04 13:54:27 -0200

    Created module for settings of alien_invasion.
    Created module for settings of alien_invasion.

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    """A class for store all settings if the
+    alien invasion"""
+
+    def __init__(self):
+        """Initializes the settings of the game."""
+        # Settings of the screen
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
